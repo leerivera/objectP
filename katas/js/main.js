c659c97da4b550d42d1d95e5480aeb3df4b76b00
leerivera <leerivera80@gmail.com>
2021-11-13 06:12:07 -0500

    solution2

--- katas/js/main.js
+++ katas/js/main.js
@@ -252,3 +252,16 @@ function setAlarm(employed, vacation){
 //8ku check for palindrone
 
 let isPalindrome=(x)=> x.toLowerCase().split('').reverse().join('') == x.toLowerCase() ? true : false 
+
+function isPalindrome(x) {
+  x = x.toLowerCase()
+  let a = 0
+  let b = x.length
+  while (a < b) {
+    if (x[a] != x[b-1])
+      return false
+    a++
+    b--
+  }
+  return true
+}
