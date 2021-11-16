126587bd05a73550f4aba4c2f5168457b86d620e
leerivera <leerivera80@gmail.com>
2021-11-16 06:12:07 -0500

    solution3

--- katas/js/main.js
+++ katas/js/main.js
@@ -244,3 +244,7 @@ const setAlarm = (employed, vacation) => employed && !vacation;
 function setAlarm(employed, vacation){
   return (employed && !vacation) ? true : false;
 }
+
+function setAlarm(employed, vacation){
+  return employed > vacation
+}
