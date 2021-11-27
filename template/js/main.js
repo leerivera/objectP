1ee9298a7bf4c422d9ed6267c69b987384142264
leerivera <leerivera80@gmail.com>
2021-11-27 06:12:07 -0500

    tow

--- template/js/main.js
+++ template/js/main.js
@@ -244,18 +244,18 @@ function removeChar(str){
 const quarterOf = m => Math.ceil(m/3);
 
 
-// const quarterOf = (month) => {
-//   if (month <= 3) {
-//     return 1
-//   } else if (month <= 6) {
-//     return 2
-//   } else if (month <= 9) {
-//     return 3
-//   } else if (month <= 12) {
-//     return 4
-//   }
+const quarterOf = (month) => {
+  if (month <= 3) {
+    return 1
+  } else if (month <= 6) {
+    return 2
+  } else if (month <= 9) {
+    return 3
+  } else if (month <= 12) {
+    return 4
+  }
   
-// }
+}
 
 const quarterOf = (month) => {
   return month < 4 ? 1 : month < 7 ? 2 : month < 10 ? 3 : 4;
