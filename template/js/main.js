b4df03898bf1f7aefd98f37d3f3c0a7ebe3f3499
leerivera <leerivera80@gmail.com>
2021-12-06 06:12:07 -0500

    s

--- template/js/main.js
+++ template/js/main.js
@@ -117,16 +117,16 @@ const updateLight = current => {
   }[current];
 }
 
-function updateLight(current) {
-  switch (current) {
-    case 'green':
-      return 'yellow';
-    case 'yellow':
-      return 'red';
-    default:
-      return 'green';
-  }
-}
+// function updateLight(current) {
+//   switch (current) {
+//     case 'green':
+//       return 'yellow';
+//     case 'yellow':
+//       return 'red';
+//     default:
+//       return 'green';
+//   }
+// }
 
 
 
