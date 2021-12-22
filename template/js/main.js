209c06e73bde976f570a5f32c637bbd393bdc68b
leerivera <leerivera80@gmail.com>
2021-12-22 07:12:07 -0500

    fake binay 8kyu

--- template/js/main.js
+++ template/js/main.js
@@ -432,3 +432,6 @@ function makeNegative(num) {
   return -Math.abs(num);
 }
 
+// fake binary
+
+// Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string
