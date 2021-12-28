c47bedc44c951722777ed68fa00ac03721b3e1d4
leerivera <leerivera80@gmail.com>
2021-12-28 08:12:10 -0500

    can we divide it

--- template/js/main.js
+++ template/js/main.js
@@ -403,3 +403,6 @@ function maps(x){
   }
   return arr;
   }
+
+  // Your task is to create functionisDivideBy (or is_divide_by) to check if an integer number is divisible by each out of two arguments.
+
