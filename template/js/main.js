5e7284baede1d5744d0ee904db26240602fd0357
leerivera <leerivera80@gmail.com>
2021-12-29 08:12:10 -0500

    solution2

--- template/js/main.js
+++ template/js/main.js
@@ -394,3 +394,12 @@ function twoSort(array) {
 function maps(x){
   return x.map(el => el *2)
 }
+
+function maps(x){
+  //return x.map(el => el * 2);
+  let arr = [];
+  for(let i = 0; i < x.length; i++){
+  arr.push(x[i] * 2);
+  }
+  return arr;
+  }
