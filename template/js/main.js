bd3372f487ce3a52903effedfd89b9f3962f0cc3
leerivera <leerivera80@gmail.com>
2022-02-14 09:12:20 -0500

    again

--- template/js/main.js
+++ template/js/main.js
@@ -363,3 +363,7 @@ function sumArray(array) {
 // Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
 
 let removeExclamationMarks = (s) => s.split('').filter(x=> x != '!' ).join('')
+
+function removeExclamationMarks(s) {
+  return s.split('!').join('');
+}
