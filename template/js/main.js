9351505031f327ed9fc22a32289601fcf6ac02e5
leerivera <leerivera80@gmail.com>
2022-02-19 10:12:20 -0500

    another

--- template/js/main.js
+++ template/js/main.js
@@ -333,3 +333,22 @@ function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
 
 // Mind the input validation.
 
+let sumArray = (array) => array == null || array.length < 3   ? 0 : array.sort((a,b)=> a-b )
+.filter((x,i) => i > 0 && i < array.length-1 )
+.reduce((acc,c)=> acc+c,0 ) 
+
+function sumArray(array) {
+  if (array == null) {
+    return 0;
+  } else if (array.length < 2) {
+    return 0;
+  } else {
+    array = array.sort(function(a,b) {return a - b;});
+    var total = 0;
+    for (var i = 1; i < array.length - 1; i++) {
+      total += array[i];
+    }
+    return total;
+  }
+}
+
