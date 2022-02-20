d8635038db370d8220a00817509123f082c469b0
leerivera <leerivera80@gmail.com>
2022-02-20 10:12:20 -0500

    high low 8kyu

--- template/js/main.js
+++ template/js/main.js
@@ -326,3 +326,10 @@ function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
   return laLigaGoals + copaDelReyGoals + championsLeagueGoals
 }
 
+
+// Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
+
+// The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+
+// Mind the input validation.
+
