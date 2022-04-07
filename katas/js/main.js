07829c883b1b411d69fe2a8eb7cd1a28a07181d5
leerivera <leerivera80@gmail.com>
2022-04-07 15:30:50 -0400

    obj

--- katas/js/main.js
+++ katas/js/main.js
@@ -3,3 +3,16 @@ var capitals = function (word) {
                .map(function(l, i) { if (l.toUpperCase() === l) return i; })
                .filter(function(i) { return i != null })
   };
+
+  let circle = {
+      radius:1,
+      location:{
+          x:1,
+          y:1
+      },
+      draw: function(){
+          console.log('draw');
+      }
+  };
+
+  circle.draw()
