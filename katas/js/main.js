547536cc56da14c3591adbb1b4ac38056d9d037e
leerivera <leerivera80@gmail.com>
2022-04-07 16:27:33 -0400

    stopwatch

--- katas/js/main.js
+++ katas/js/main.js
@@ -1,18 +1,49 @@
-var capitals = function (word) {
-    return word.split('')
-               .map(function(l, i) { if (l.toUpperCase() === l) return i; })
-               .filter(function(i) { return i != null })
-  };
+// var capitals = function (word) {
+//     return word.split('')
+//                .map(function(l, i) { if (l.toUpperCase() === l) return i; })
+//                .filter(function(i) { return i != null })
+//   };
 
-  let circle = {
-      radius:1,
-      location:{
-          x:1,
-          y:1
-      },
-      draw: function(){
-          console.log('draw');
-      }
-  };
+//   let circle = {
+//       radius:1,
+//       location:{
+//           x:1,
+//           y:1
+//       },
+//       draw: function(){
+//           console.log('draw');
+//       }
+//   };
 
-  circle.draw()
+//   circle.draw()
+
+const stopWatch = new Stopwatch(){
+    let startTime, endTime, running, duration = 0;
+
+    this.start = function() {
+        if(running)
+           throw new Error("You already pressed this button, press another");
+           running = true;
+           startTime = new Date();
+    };
+
+    this.stop = function() {
+        if(!running){
+            throw new Error('Stop watch is off bruh')
+        } 
+        running = false;
+        endTime = new Date();
+
+        const seconds = (endTime.getTime() - startTime.getTime())/ 1000;
+        duration += seconds;
+
+    };
+
+    this.reset = function() {
+        startTime = null;
+        endTime = null;
+        running = null;
+        duration = 0;
+    }
+
+}
