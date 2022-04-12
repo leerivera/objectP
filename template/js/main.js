3deb20eb11cfbbb8de7e7398f72db2404c937501
leerivera <leerivera80@gmail.com>
2022-04-12 11:06:13 -0400

    typeof

--- template/js/main.js
+++ template/js/main.js
@@ -31,3 +31,7 @@ function multiplyNumeric(object){
     }
 }
 
+function filter_list(l) {
+    // Return a new array with the strings filtered out
+  return l.filter( str => typeof str != 'string');
+  }
