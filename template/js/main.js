637187198c3547875c6ef391cc1a55d3e6827217
leerivera <leerivera80@gmail.com>
2022-04-17 10:45:33 -0400

    century

--- template/js/main.js
+++ template/js/main.js
@@ -77,3 +77,13 @@ function calculateYears(principal, interest, tax, desired) {
     }
     return years;
   }
+
+
+  // The first century spans from the year 1 up to and including the year 100, the second century 
+  // - from the year 101 up to and including the year 200, etc.
+
+
+  function century(year) {
+    // Finish this :)
+    return  Math.ceil(year/100)
+  }
