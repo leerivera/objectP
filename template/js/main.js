5e651bbecf9025702bc1ea6f91aa0d3892ad0c43
leerivera <leerivera80@gmail.com>
2022-05-03 13:01:20 -0400

    diffrence

--- template/js/main.js
+++ template/js/main.js
@@ -128,13 +128,18 @@ function updateLight(current) {
   }
 }
 
+
+
+
+
 //   Write a function that takes a list of strings as an argument and returns a filtered list containing the same elements but with the 'geese' removed.
 
 // The geese are any strings in the following array, which is pre-populated in your
 
 // Long Solution
 
-var birds = [ 'Mallard',
+var birds = [ 
+  'Mallard',
   'Hook Bill',
   'African',
   'Crested',
@@ -159,3 +164,24 @@ function gooseFilter (birds) {
   var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
   return birds.filter( bird => geese.indexOf(bird) < 0 );
 };
+
+
+// In this simple exercise, you will create a program that will take two lists of integers, a and b. Each list will consist of 3 positive integers above 0, representing the dimensions of cuboids a and b. You must find the difference of the cuboids' volumes regardless of which is bigger.
+
+// For example, if the parameters passed are ([2, 2, 3], [5, 4, 1]), the volume of a is 12 and the volume of b is 20. Therefore, the function should return 8.
+
+// Your function will be tested with pre-made examples as well as random ones.
+
+// If you can, try writing it in one line of code.
+const calculateVolume = (cuboid = [1, 1, 1]) =>
+  cuboid.reduce((total, value) => total * value, 1)
+
+const findDifference = (a, b) =>
+  Math.abs(calculateVolume(a) - calculateVolume(b))
+
+// Function Export
+module.exports = findDifference
+
+function find_difference(a, b) {
+  return Math.abs(a.reduce((previous, current) => previous * current) - b.reduce((previous, current) => previous * current));
+}
