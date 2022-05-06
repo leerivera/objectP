5ed8b3a6728966a9bec34f090974f1e28b4816c0
leerivera <leerivera80@gmail.com>
2022-05-06 17:04:34 -0400

    find the area

--- katas/js/main.js
+++ katas/js/main.js
@@ -75,3 +75,11 @@ const max = list => Math.max(...list)
 // Function Export
 module.exports = { min, max }
 
+// You are given the length and width of a 4-sided polygon. The polygon can either be a rectangle or a square.
+// If it is a square, return its area. If it is a rectangle, return its perimeter.
+
+const areaOrPerimeter = function(l , w) {
+    // Return your answer
+    return l == w ? w * l : l * 2 + w * 2
+  };
+
