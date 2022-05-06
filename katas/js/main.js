a4423d8755a5fac7030ca106e70eb2a797f189be
leerivera <leerivera80@gmail.com>
2022-05-06 17:17:17 -0400

    sq

--- katas/js/main.js
+++ katas/js/main.js
@@ -83,3 +83,13 @@ const areaOrPerimeter = function(l , w) {
     return l == w ? w * l : l * 2 + w * 2
   };
 
+//   Complete the square sum function
+//    so that it squares each number passed into it and then sums the results together.
+
+function squareSum(numbers) {
+    let sum = 0
+    for( let i = 0; i < numbers.length; i++) {
+        sum += numbers[i] **2
+    }
+    return sum
+}
