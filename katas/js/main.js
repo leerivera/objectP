41bc0c66ba0984daed0b1ceb908a5d8a12ad878d
leerivera <leerivera80@gmail.com>
2022-05-08 14:55:45 -0400

    expressions matter

--- katas/js/main.js
+++ katas/js/main.js
@@ -133,3 +133,24 @@ function hoopCount (n) {
  function checkForFactor (base, factor) {
     return base % factor === 0;
   }
+
+
+
+//   Given three integers a ,b ,c, return the largest number obtained after inserting the following operators and brackets: +, *, ()
+// In other words , try every combination of a,b,c with [*+()] , and return the Maximum Obtained
+
+const expressionMatter = (a, b, c) => Math.max(a + b + c, a * b * c, (a + b) * c, a * (b + c))
+
+// Export
+module.exports = expressionMatter
+
+function expressionMatter(a, b, c) {
+    let arr = [];
+    arr.push(a + b + c);
+    arr.push(a * b * c);
+    arr.push(a + b * c);
+    arr.push(a * b + c);
+    arr.push((a + b) * c);
+    arr.push(a * (b + c));
+    return arr.reduce((a,b) => Math.max(a, b));
+  }
