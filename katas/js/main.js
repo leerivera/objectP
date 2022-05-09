66b237529a9acb2701d8a77ce9a69b2ec3bf8e57
leerivera <leerivera80@gmail.com>
2022-05-09 12:18:44 -0400

    basic operators

--- katas/js/main.js
+++ katas/js/main.js
@@ -154,3 +154,40 @@ function expressionMatter(a, b, c) {
     arr.push(a * (b + c));
     return arr.reduce((a,b) => Math.max(a, b));
   }
+
+
+
+//   Your task is to create a function that does four basic mathematical operations.
+
+//   The function should take three arguments - operation(string/char), value1(number), value2(number).
+//   The function should return result of numbers after applying the chosen operation.
+
+
+
+
+
+
+
+
+  function basicOp(operation, value1, value2)
+{
+  
+  switch (operation) {
+    case "+":
+      result = value1 + value2;
+      break;
+    case "-":
+      result = value1 - value2;
+      break;
+    case "*":
+      result = value1 * value2;
+      break;
+    case "/":
+      result = value1 / value2;
+      break;
+    default:
+      return null;
+  }
+  return result;
+}
+
