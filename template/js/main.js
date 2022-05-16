0afef34d49db3180846ac10cc756595451db9a0d
leerivera <leerivera80@gmail.com>
2022-05-15 20:12:20 -0400

    average mean mean

--- template/js/main.js
+++ template/js/main.js
@@ -265,3 +265,14 @@ const quarterOf = (month) => {
 // calculate avg of an array
 
 let find_average = (array) => array.length > 0 ? array.reduce((acc,c)=> acc + c, 0)/array.length : 0
+
+function find_average(array) {
+  if (array.length === 0) {
+  return 0;
+  }
+  var result = 0;
+  for (i=0; i<array.length; i++) {
+    result +=array[i];
+  }
+  return result/array.length;
+}
