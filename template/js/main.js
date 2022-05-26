5b8f846ba2ef6007cdd06edbbc58285d02ffbdcb
leerivera <leerivera80@gmail.com>
2022-05-26 13:15:09 -0400

    solution

--- template/js/main.js
+++ template/js/main.js
@@ -435,3 +435,7 @@ function makeNegative(num) {
 // fake binary
 
 // Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string
+
+function fakeBin(x) {
+  return x.replace(/\d/g, d => d < 5 ? 0 : 1);
+}
