e016477706307ebe140b200e820f885f90898050
leerivera <leerivera80@gmail.com>
2022-05-28 23:23:13 -0400

    count sheep 8kyu

--- katas/js/main.js
+++ katas/js/main.js
@@ -326,3 +326,6 @@ function invert(array) {
   }
    return newArr;
 }
+
+// 8kyu if you cant sleep count sheep
+// Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
